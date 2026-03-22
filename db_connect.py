0efca9dc60a140c84a1677c91617c4d45653631a
Sreeram <ram@RAMs-MacBook-Air-2.local>
2026-03-22 02:37:28 -0400

    added frontend changes

--- db_connect.py
+++ db_connect.py
@@ -1,18 +1,20 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+import os
 
-USER = "root"
-PASSWORD = "password"
-HOST = "127.0.0.1"
-PORT = 3306
-DB_NAME = "shopper-DB"
+from dotenv import load_dotenv
+load_dotenv()
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+USER     = os.environ.get("DB_USER", "root")
+PASSWORD = os.environ.get("DB_PASSWORD", "password")
+HOST     = os.environ.get("DB_HOST", "127.0.0.1")
+PORT     = os.environ.get("DB_PORT", "3306")
+DB_NAME  = os.environ.get("DB_NAME", "shopper-DB")
 
 DB_URL = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
 
 db_engine = create_engine(DB_URL)
-
-db_connection = db_engine.connect()
 
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
 
